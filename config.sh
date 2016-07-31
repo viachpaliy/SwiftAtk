b53ec0b04ed7a7bb111625fbfedef12fe170113f
Rene Hexel <r.hexel@griffith.edu.au>
2016-07-31 14:24:40 +1000

    Compile with Swift 3.0 preview 3

--- config.sh
+++ config.sh
@@ -7,6 +7,6 @@ Mod=Atk
 Module=${Mod}-1.0
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
 mod=`echo "${Mod}" | tr '[:upper:]' '[:lower:]'`
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+export PATH=`pwd`/.build/gir2swift/.build/debug:"${PATH}"
 LINKFLAGS=`pkg-config --libs $mod gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//g' -e 's/ -pthread/ -lpthread/g' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $mod gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
