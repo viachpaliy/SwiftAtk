7b24ff6a5c75f5262e025ce06b89e36b1b66ef63
viachpaliy <viachpaliy@ukr.net>
2018-06-17 10:21:35 +0300

    change repository from original to fork

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Atk",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CAtk.git", majorVersion: 1),
-        .Package(url: "https://github.com/rhx/SwiftGObject.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/CAtk.git", majorVersion: 1),
+        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 2)
     ],
     swiftLanguageVersions: [3, 4]
 )
