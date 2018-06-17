f34b376018e4fa2fed6a991d14d658ad1e556e1b
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:27:30 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Atk",
     dependencies: [
         .Package(url: "https://github.com/viachpaliy/CAtk.git", majorVersion: 1),
-        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 1)
     ],
     swiftLanguageVersions: [3, 4]
 )
